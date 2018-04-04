fa66c6e459093436343b48d80f5219b10a578524
Kamila2908 <36546067+Kamila2908@users.noreply.github.com>
2018-04-04 17:47:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ var b = 1;
 value = (a * a) - (2 * a * b) - (b * b);
 
 if (value > 0) {
-    console.log('Wynik dodatni')
+    console.log('Wynik dodatni');
 } else {
-    console.log('Wynik ujemny')
+    console.log('Wynik ujemny');
 }
 
 switch (value) {
     case 0:
-    console.log('Wynik równa się zero')
+    console.log('Wynik równa się zero');
 }
